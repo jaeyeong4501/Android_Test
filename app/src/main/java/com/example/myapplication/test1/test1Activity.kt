958cc43cbe1d4a8841a4d13771611973bb70d87e
이재영 <139414509+jaeyeong4501@users.noreply.github.com>
2023-10-25 16:30:16 +0900

    다시 커밋

--- app/src/main/java/com/example/myapplication/test1/test1Activity.kt
+++ app/src/main/java/com/example/myapplication/test1/test1Activity.kt
@@ -1,12 +1,23 @@
 package com.example.myapplication.test1
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.MainActivity
 import com.example.myapplication.R
 
 class test1Activity : AppCompatActivity() {
+
+
+    //인트로 코드
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test1)
+        setContentView(R.layout.activity_test1) //activity_test1 파일로 맨처음 보여짐
+
+        SystemClock.sleep(3000) //3 초후 화면 전환
+    val intent =Intent(this,MainActivity::class.java)
+        startActivity(intent) // MainActivity 이동
+        finish()
     }
 }
