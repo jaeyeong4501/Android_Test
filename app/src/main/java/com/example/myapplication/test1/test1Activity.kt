5daac316c79c409ec014255a1ac2295f1db50863
이재영 <139414509+jaeyeong4501@users.noreply.github.com>
2023-10-25 15:26:53 +0900

    새로운 폴더

---
+++ app/src/main/java/com/example/myapplication/test1/test1Activity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.test1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class test1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test1)
+    }
+}
